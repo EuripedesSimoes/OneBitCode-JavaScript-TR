d7639172e3852a8f281024f25c0e16d3c836b0a0
Eurípedes simões luz júnior <esimoeslj02@gmail.com>
2024-07-26 16:49:27 -0400

    Music Player
    Atualização do Tocador de Audio: Dia 26-07-2024

---
+++ Js - Music Player/music_player.js
@@ -0,0 +1,8 @@
+const num_musics = 3
+const PP = document.getElementById('Play_Pause')
+const MA = document.getElementById('music_audio')
+
+function play(){
+    MA.play()}
+
+PP.addEventListener('click', play) 
