4fafa73ecc1de1db4d1d0f80cd94a681452ba545
Eurípedes simões luz júnior <esimoeslj02@gmail.com>
2024-08-31 11:53:00 -0400

    Music Player
    Att 01 - 31/08/2024

--- Js - Music Player/funcoes.js
+++ Js - Music Player/funcoes.js
@@ -1,4 +1,4 @@
 export const music_origin = document.querySelector('.music_origin')
-export const msc_org = ['','Doom Eternal', 'The Living Tombstone', 'Pentakill' ]
+export const msc_org = ['','Doom Eternal', 'Devil May Cry 5', 'Pentakill' ]
 export const music_name = document.querySelector('.music_name')
-export const msc_name = ['','The Only Thing They Fear Is You','My Ordinary Life','Mortal Reminder']
+export const msc_name = ['','The Only Thing They Fear Is You','Bury The Light','Mortal Reminder']
